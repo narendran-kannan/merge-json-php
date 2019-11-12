80526e4fe7518caec41e337019635675ebad9dc3
Narendran Kannan <nashkannan35@gmail.com>
2019-11-12 21:26:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 function mergejson($foldername, $inputbasename, $outputbasename, $maxsize)
 {
+  //Count Files in the input folder
   $fi = new FilesystemIterator($foldername.'/', FilesystemIterator::SKIP_DOTS);
   $merged = [];
   $merged_json = [];
 
-
+  //Decoding Each File
   for ($i = 1; $i <= iterator_count($fi); $i++) {
     $aDecoded = json_decode(file_get_contents($foldername . '/' . $inputbasename . $i . '.json'), true);
 
@@ -13,9 +14,11 @@ function mergejson($foldername, $inputbasename, $outputbasename, $maxsize)
     if ($i == 1) {
       $exist = array($name => [],);
     }
+    //Decoding respective to key value
     $merged = [
       $name => array_merge($aDecoded[$name]),
     ];
+    //Merging existing array to newly Decoded array 
     $exist = [
       $name => array_merge($exist[$name], $merged[$name]),
     ];
@@ -24,6 +27,7 @@ function mergejson($foldername, $inputbasename, $outputbasename, $maxsize)
   $merged_json = json_encode($exist, JSON_PRETTY_PRINT);
 
   if (sizeofvar($merged_json) < $maxsize) {
+    //Creating File
     $mergedfile = fopen('output/'.$outputbasename.".json", "w") or die("Unable to open file!");
     fwrite($mergedfile, $merged_json);
 
@@ -33,9 +37,9 @@ function mergejson($foldername, $inputbasename, $outputbasename, $maxsize)
     echo "Merged File size is larger than given size! The file cannot be created";
   }
 }
-
+//Function Call
 mergejson('input', 'data', 'output', '520');
-
+//To find the size of merged Json
 function sizeofvar($var)
 {
   $start_memory = memory_get_usage();
